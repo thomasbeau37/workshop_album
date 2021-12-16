f9663ae89e8b6a7d341b66cd523b8dfbf8dcefef
Sovan <sovanlong@gmail.com>
2021-12-16 10:40:21 +0100

    webpack copy change

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,8 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: ':assets' }
+        { from: 'assets' ,
+          to: "assets/"}
       ]
     })
   ],
