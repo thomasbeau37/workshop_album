a91e192082a4cd389e32cf491699b263a8667712
Toto <thomas.beau37@hotmail.fr>
2021-12-17 11:18:05 +0100

    push

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const bundleName = 'webgl';
 
 module.exports = {
@@ -47,12 +46,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `${bundleName}.css`
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'assets' ,
-          to: "assets/"}
-      ]
-    })
   ],
   devServer: {
     contentBase: './',
